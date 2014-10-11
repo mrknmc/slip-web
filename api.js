25067c3f73d16d8c569f67a6e1abbe67a44244a7
Mark Nemec <mrknmc@me.com>
2014-10-11 21:09:44 +0100

    added content-type and accept headers

--- api.js
+++ api.js
@@ -7,6 +7,10 @@ var router = express.Router();
 
 router.route('/measurements')
 .get(function(req, res) {
+  if (!req.accepts('application/json')) {
+    // check json header
+    return res.sendStatus(406);
+  }
   var query = models.Measurement.find(function (err, measurements) {
     if (err) {
       return res.json(err);
@@ -16,6 +20,9 @@ router.route('/measurements')
   });
 })
 .post(function(req, res) {
+  if (!req.is('application/json')) {
+    return res.sendStatus(415);
+  }
   var measurements = req.body;
   var errors = [];
   _.each(measurements, function(measurement) {
@@ -28,7 +35,7 @@ router.route('/measurements')
     });
   });
   if (errors.length === 0) {
-    res.status(201).end();
+    res.sendStatus(201);
   } else {
     // return all errors at once
     res.status(400).json(errors);
