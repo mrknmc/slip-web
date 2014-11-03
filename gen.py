4b683ffc14259dfb923c079e7157b90e8ecdc5a2
Mark Nemec <mrknmc@me.com>
2014-11-03 21:09:55 +0000

    added script that generates random data

---
+++ gen.py
@@ -0,0 +1,36 @@
+import time
+import random
+
+DIODE_COUNT = 24
+
+
+def days_to_secs(days):
+    return days * 60 * 60 * 24
+
+
+def main(days=7, freq=3600):
+
+    template = """{{
+    "windSpeed": {wind_speed},
+    "lightIntensities": {light_intensities},
+    "timestamp": {timestamp},
+    "windDirection": 2
+}},"""
+
+    timestamp = time.time() - days_to_secs(days + 1)
+
+    # for x days
+    for i in xrange(days_to_secs(days) / freq):
+        # every y seconds
+        timestamp = timestamp + freq
+        d = {
+            'wind_speed': random.gauss(40, 3),
+            'light_intensities': [int(random.gauss(100, 20)) for i in xrange(DIODE_COUNT)],
+            'timestamp': timestamp,
+        }
+
+        print(template.format(**d))
+
+
+if __name__ == '__main__':
+    main()
