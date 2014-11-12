5729d6cd668bad1b8f9562ab6d62de5e6c97669d
Mark Nemec <mrknmc@me.com>
2014-11-12 10:46:06 +0000

    fixed prettyLocation lookup

--- server/location.js
+++ server/location.js
@@ -4,9 +4,9 @@ var ONE_DAY = 86400;
 
 
 exports.findByCoords = function(req, res) {
-  prettyLocation(req.query.xCoord, req.query.yCoord, function(err, loc) {
+  getLocation(req.query.xCoord, req.query.yCoord, function(err, loc) {
     if (loc) {
-      res.json(address);
+      res.json(loc);
     } else {
       res.status(418).json({'error': 'Could not retrieve coordinates from MapQuest.'});
     }
@@ -14,18 +14,7 @@ exports.findByCoords = function(req, res) {
 };
 
 
-exports.prettyLocation = function(data) {
-  if (data.results.length > 0) {
-    if (data.results[0].locations.length > 0) {
-      var loc = data.results[0].locations[0];
-      return loc.street + ', ' + loc.adminArea5 + ', ' + loc.adminArea3;
-    }
-  }
-  return null;
-};
-
-
-exports.getLocation = function(xCoord, yCoord, callback) {
+function getLocation(xCoord, yCoord, callback) {
   var coords = xCoord + ',' + yCoord;
 
   redisClient.get(coords, function(err, reply) {
@@ -43,12 +32,36 @@ exports.getLocation = function(xCoord, yCoord, callback) {
           json: true,
         }, function (error, response, body) {
           if (!error && response.statusCode == 200) {
-            redisClient.setex(coords, ONE_DAY, JSON.stringify(body));
-            callback(null, body);
+            if (prettyLocation(data)) {
+              // if pretty location exists store body
+              redisClient.setex(coords, ONE_DAY, JSON.stringify(body));
+              callback(null, body);
+            } else {
+              // otherwise print error
+              if (data.messages.length > 0) {
+                callback(new Error(data.messages[0]), null);
+              } else {
+                callback(new Error('Unspecified MapQuest Error.'), null);
+              }
+            }
           } else {
             callback(error, null);
           }
       });
     }
   });
-};
+}
+
+
+function prettyLocation(data) {
+  if (data.results.length > 0) {
+    if (data.results[0].locations.length > 0) {
+      var loc = data.results[0].locations[0];
+      return loc.street + ', ' + loc.adminArea5 + ', ' + loc.adminArea3;
+    }
+  }
+  return null;
+}
+
+exports.getLocation = getLocation;
+exports.prettyLocation = prettyLocation;
